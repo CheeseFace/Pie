e9a346c531ab79db4ef42cc04d0c5cd8964fcc69
CheeseFace <S.E.George@reading.ac.uk>
2015-03-23 14:55:08 +0000

    first commit

---
+++ otest.f90
@@ -0,0 +1,8 @@
+program irate
+
+integer :: anger = 0
+
+write(*,*) anger-1
+! Adjusted
+
+end program irate
---
+++ test.f90
@@ -0,0 +1,7 @@
+program irate
+
+integer :: anger = 0
+
+write(*,*) anger
+
+end program irate
